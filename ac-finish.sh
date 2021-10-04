fd461f7def5681fc9670180b7726512882d64118
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-10-05 07:59:10 +0900

    Fixed

--- ac-finish.sh
+++ ac-finish.sh
@@ -4,4 +4,3 @@ gh pr merge
 echo ""$(git symbolic-ref --short HEAD)" Finished. Good Work \U1F619"
 CONTEST_BRANCH=$(git symbolic-ref --short HEAD)
 git checkout master
-git branch --delete $CONTEST_BRANCH
