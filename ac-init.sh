b7147ada300b4f5218ef9aeacd52a333393e71ca
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-10-15 09:20:00 +0900

    Fixed

--- ac-init.sh
+++ ac-init.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-mkdir $1
 
 if git branch | grep $1; then
     echo "$1 already exist 😏"
@@ -8,7 +7,7 @@ else
     echo "$1 does not exist 😋"
     git checkout -b $1
 fi
-
+mkdir $1
 git commit --allow-empty -m "Start $1 Contest"
 gh pr create --base master --title "$1 Contest" --body "Let's Solve $1 Contest!"
 code $1
