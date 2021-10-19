5551cb84ccbd776be0a1aaa1375a33498010a59d
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-10-19 10:34:29 +0900

    FIxed

--- ac-retry.sh
+++ ac-retry.sh
@@ -2,12 +2,11 @@
 
 if git branch | grep $1; then
     echo "$1 already exist 😏"
-    exit 0
+    git checkout $1
 else
     echo "$1 does not exist 😋"
-    git checkout -b $1
+    exit 0
 fi
-mkdir $1
 git commit --allow-empty -m "Start $1 Contest"
 gh pr create --base master --title "$1 Contest" --body "Let's Solve $1 Contest!"
 code $1
