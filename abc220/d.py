ece1be7b717185234307b980c36f75eebc2292b9
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-11-15 20:01:13 +0900

    Solved  abc220 d

--- abc220/d.py
+++ abc220/d.py
@@ -1,14 +1,16 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-dp = [[i for i in range(10)] for _ in range(N+1)]
+dp = [[0 for i in range(10)] for _ in range(N)]
 
-dp[1][A[1]] = 1
+dp[0][A[0]] = 1
 
-for i in range(1,N):
+MOD = 998244353
+
+for i in range(0,N-1):
     for j in range(10):
-        dp[i+1][(j+A[i])%10] += dp[i][j]
-        dp[i+1][(j*A[i])%10] += dp[i][j]
+        dp[i+1][(j+A[i+1])%10] += dp[i][j] % MOD
+        dp[i+1][(j*A[i+1])%10] += dp[i][j] % MOD
 
 for i in range(10):
-    print(dp[N][i])
+    print(dp[N-1][i] % MOD)
