46cc10be3d359126834976c81098aa34a2c4ae44
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-12-01 20:08:37 +0900

    Done

--- abc229/c.py
+++ abc229/c.py
@@ -1,4 +1,21 @@
 N,W = map(int,input().split())
 
+cheese = []
+for _ in range(N):
+    A,B = map(int,input().split())
+    cheese.append([A,B])
 
+cheese.sort(reverse=True)
 
+ans = 0
+for item in cheese:
+    A,B= item
+    if B <= W:
+        ans += A * B
+        W -= B
+    else:
+        ans += A * W
+        W = 0
+        break
+
+print(ans)
