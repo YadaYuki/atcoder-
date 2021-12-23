345c57cac689ad4b2980d8b06d281bf916d0937a
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-12-23 17:50:49 +0900

    Fixed

--- ac-failed.sh
+++ ac-failed.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 dir=$(dirname $(which $0))
-poetry run python ${dir}/failed.py $(git symbolic-ref --short HEAD) $1
+poetry run python ./${dir}/failed.py $(git symbolic-ref --short HEAD) $1
