5de2a4926dd2f8193fd8015500010715bfdcf5e2
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-12-23 18:33:47 +0900

    Solved  arc131 c

--- arc131/c.py
+++ arc131/c.py
@@ -1,2 +1,19 @@
 N = int(input())
 A = list(map(int,input().split()))
+
+if N % 2 == 1:
+    print('Win')
+    exit()
+
+v = 0
+
+
+for i in range(N):
+     v ^= A[i]
+for i in range(N):
+    if v == A[i]:
+        print('Win')
+        exit()
+    
+print('Lose')
+    
