25b21f58a23ef1910fbf812b865048d6caea0917
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-12-25 22:45:45 +0900

    Failed to solve  d 😢

--- shell/failed.py
+++ shell/failed.py
@@ -3,7 +3,7 @@ import sys
 import datetime
 import pandas as pd
 from os.path import dirname,join
-from const import PATH_TO_FAILED_CSV
+from const import PATH_TO_FAILED_CSV,KEYWORDS
 
 
 if __name__ == "__main__":
@@ -14,12 +14,14 @@ if __name__ == "__main__":
 
     keyword_idx = -1
     keywords = []
-    while keyword_idx != 0:
+    while True:
         print(f'0: No')
         for i,keyword in enumerate(KEYWORDS):
             print(f'{i+1}:{keyword}')
         print("Input keyword idx:")
         keyword_idx = int(input())
+        if keyword_idx == 0:
+            break
         keywords.append(KEYWORDS[keyword_idx-1])
 
     df = pd.read_csv(PATH_TO_FAILED_CSV)
