7deb42841cbc1acdeeaae23b2ec6c7c563f49ec1
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-12-25 22:39:14 +0900

    Fixed

--- shell/ac-solved.sh
+++ shell/ac-solved.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # NOTE: $1は小文字のアルファベット一文字。
 CONTEST_NAME=$(git symbolic-ref --short HEAD)
-poetry run python $AC_ROOT/shell/solve.py $CONTEST_NAME $1
+poetry run python $AC_ROOT/shell/solved.py $CONTEST_NAME $1
 git add $AC_ROOT/$CONTEST_NAME/* $AC_ROOT/csv/solved.csv 
 git commit -m "Solved  $CONTEST_NAME $1"
 echo "You solved $1. Well Done 👍"
