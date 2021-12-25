4da9ce6de0d6dd59c2a40c39631e698fd8c0dae1
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-12-25 17:26:04 +0900

    Added

--- shell/ac-solved.sh
+++ shell/ac-solved.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # NOTE: $1は小文字のアルファベット一文字。
 CONTEST_NAME=$(git symbolic-ref --short HEAD)
-git add $AC_ROOT/$CONTEST_NAME/$1.py # TODO: Python以外の言語にも対応する
+poetry run python $AC_ROOT/shell/solve.py $CONTEST_NAME $1
+git add $AC_ROOT/$CONTEST_NAME/* $AC_ROOT/csv/solved.csv 
 git commit -m "Solved  $CONTEST_NAME $1"
 echo "You solved $1. Well Done 👍"
--- shell/failed.py
+++ shell/failed.py
@@ -1,19 +1,27 @@
 import csv
 import sys
 import datetime
-from os.path import dirname
 import pandas as pd
 from os.path import dirname,join
+from const import PATH_TO_FAILED_CSV
 
-DIR = dirname(__file__)
-AC_ROOT = join(DIR,'..')
 
 if __name__ == "__main__":
     args = sys.argv
     branch_name =  args[1]
     problem_name = args[2]
-    filename = f'{branch_name}/{problem_name}.py'
-    PATH_TO_CSV = f'{AC_ROOT}/failed.csv'
-    df = pd.read_csv(PATH_TO_CSV)
-    df = df.append({'problem': filename, 'date':datetime.date.today(),'is_resolved': False,'resolved_at':None}, ignore_index=True)
-    df.to_csv(PATH_TO_CSV, index=False)
+    filename = f'{branch_name}/{problem_name}'
+
+    keyword_idx = -1
+    keywords = []
+    while keyword_idx != 0:
+        print(f'0: No')
+        for i,keyword in enumerate(KEYWORDS):
+            print(f'{i+1}:{keyword}')
+        print("Input keyword idx:")
+        keyword_idx = int(input())
+        keywords.append(KEYWORDS[keyword_idx-1])
+
+    df = pd.read_csv(PATH_TO_FAILED_CSV)
+    df = df.append({'problem': filename, 'date':datetime.date.today(),'keyword':';'.join(keywords),'is_resolved': False,'resolved_at':None}, ignore_index=True)
+    df.to_csv(PATH_TO_FAILED_CSV, index=False)
