151aef0b6b29639a38c6e1e2ea3d3bd11444d331
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-01-13 11:26:40 +0900

    Resolved abc221 d3

--- abc221/d3.py
+++ abc221/d3.py
@@ -1,2 +1,24 @@
 N = int(input())
-ans = [0] * N # ans[k]=1~N人がログインしていた日数
+ans = [0] * (N+1) # ans[k]=1~N人がログインしていた日数
+LOGIN, LOGOUT = 1, 0
+login_logout_day = []
+for _ in range(N):
+    A,B = map(int,input().split())
+    login_logout_day.append([A,LOGIN])    
+    login_logout_day.append([A+B,LOGOUT])
+
+login_logout_day.sort()
+
+cur_login_user_num = 0
+for i in range(2*N-1):
+    d,l = login_logout_day[i]
+    if l == LOGIN:
+        cur_login_user_num += 1
+        ans[cur_login_user_num] += login_logout_day[i+1][0] - d
+    else:
+        cur_login_user_num -= 1
+        ans[cur_login_user_num] += login_logout_day[i+1][0] - d
+
+
+print(*ans[1:])
+
