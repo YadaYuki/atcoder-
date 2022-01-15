04744a0d61ecd2cfc6a6d4865a1dfc009aacdc09
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-01-15 21:22:30 +0900

    Solved  abc235 c

--- abc235/c.py
+++ abc235/c.py
@@ -1,8 +1,19 @@
-
+from collections import defaultdict
 N,Q = map(int,input().split())
 
 a = list(map(int,input().split()))
 
-a_map = {} # {i:[iが登場する位置]}
+a_map = defaultdict(list) # {i:[iが登場する位置]}
 
+for i in range(N):
+    a_map[a[i]].append(i)
 
+query = [list(map(int,input().split())) for _ in range(Q)]
+
+for i in range(Q):
+    x,k = query[i]
+    if len(a_map[x]) < k:
+        print(-1)
+    else:
+        print(a_map[x][k-1]+1)
+    
