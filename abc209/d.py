35340c045c2d8f6c69b493659df70ef7c95a2a52
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-01-30 23:27:11 +0900

    Solved  abc209 d2

--- abc209/d.py
+++ abc209/d.py
@@ -6,3 +6,10 @@ for _ in range(N-1):
     a,b = map(int, input().split())
     graph[a-1].append(b-1)
     graph[b-1].append(a-1)
+
+query = []
+
+for _ in range(N-1):
+    a,b = map(int, input().split())
+    graph[a-1].append(b-1)
+    graph[b-1].append(a-1)
