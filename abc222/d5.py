b045c14431671756b785d44365e7dc313fcf811e
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-02-10 09:44:00 +0900

    Solved  abc222 d5

--- abc222/d5.py
+++ abc222/d5.py
@@ -1,7 +1,8 @@
+
 N = int(input())
 a = list(map(int,input().split()))
 b = list(map(int,input().split()))
-
+MOD = 998244353
 max_b = max(b)
 R = [[0 for _ in range(max_b+1)] for _ in range(N)]
 
@@ -9,15 +10,15 @@ R = [[0 for _ in range(max_b+1)] for _ in range(N)]
 
 for j in range(max_b+1):
     if a[0] <= j <= b[0]:
-        R[0][j] = R[0][j-1] + 1
+        R[0][j] = (R[0][j-1] + 1) % MOD
     else:
         R[0][j] = R[0][j-1]
 
 
-for i in range(0,N):
+for i in range(1,N):
     for j in range(max_b+1):
         if a[i] <= j <= b[i]:
-            R[i][j] = R[i][j-1] + R[i-1][j]
+            R[i][j] = (R[i][j-1] + R[i-1][j]) % MOD
         else:
             R[i][j] = R[i][j-1]
 
