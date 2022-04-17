571aec0161c83d1ff411aab4f7bd79eaa029fc2e
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-04-17 13:24:11 +0900

    Solved  abc240 e

---
+++ abc240/e.py
@@ -0,0 +1,42 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+N = int(input())
+tree = [[] for _ in range(N)]
+for _ in range(N-1):
+  u,v = map(int,input().split())
+  tree[u-1].append(v-1)
+  tree[v-1].append(u-1)
+
+
+def dfs(p,n):
+  global id
+  if len(tree[n]) == 1 and n != 0:
+    LR[n] = [id,id]
+    id += 1
+  else:
+    for i in tree[n]:
+      if i != p:
+        dfs(n,i)
+
+    r = tree[n][(len(tree[n])-1)]
+    l = tree[n][0]
+    
+    if r == p:
+      r = tree[n][(len(tree[n])-2)]
+    if l == p:
+      l = tree[n][1]
+    LR[n] = [LR[l][0],LR[r][1]]
+    
+
+LR = [None] * N
+id = 1
+
+dfs(-1,0)
+
+
+
+
+for i in range(N):
+  print(LR[i][0],LR[i][1])
+
