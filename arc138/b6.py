772680c0e0eed3592cb79354578e1449dff50314
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-06-01 11:53:04 +0900

    Solved  arc138

--- arc138/b6.py
+++ arc138/b6.py
@@ -1,12 +1,14 @@
+from collections import deque
+
 N = int(input())
-A = list(map(int, input().split()))
+A = deque(list(map(int, input().split())))
 flip_flag = 0
 
 while len(A):
     if A[-1] == flip_flag:
         A.pop()
     elif A[0] == flip_flag:
-        A.pop(0)
+        A.popleft()
         flip_flag = 1 - flip_flag
     else:
         print('No')
