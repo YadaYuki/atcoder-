6a4de91a7436df5daacb63c4b6215fad96c6ddc7
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-06-18 16:56:11 +0900

    Solved  arc124 b2

--- arc124/b.py
+++ arc124/b.py
@@ -4,19 +4,22 @@ a = list(map(int,sys.stdin.readline().split()))
 b = list(map(int,sys.stdin.readline().split()))
 x_to_idx_set_map = {}
 
+
 for i in range(N):
     for j in range(N):
         x = a[i] ^ b[j]
         if x in x_to_idx_set_map:
-            if not (j in x_to_idx_set_map[x]):
-                x_to_idx_set_map[x].add(j)
+            if not (x_to_idx_set_map[x][j]):
+                x_to_idx_set_map[x][j] = True
         else:
-            x_to_idx_set_map[x] = set([j])
+            x_to_idx_set_map[x] = [False] * N
+            x_to_idx_set_map[x][j] = True
+
 
 
 ans = []
 for k,v in x_to_idx_set_map.items():
-    if len(v) == N:
+    if sum(v) == N:
         ans.append(k)
 ans.sort()
 print(len(ans))
