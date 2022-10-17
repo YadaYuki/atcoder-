a7cb1a9e0df8ea9a0a25e694e352c5d46111fc1f
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-10-17 14:00:03 +0900

    Solved  kyopro-tessoku a

--- kyopro-tessoku/a59_2.cpp
+++ kyopro-tessoku/a59_2.cpp
@@ -10,12 +10,13 @@ private:
 public:
     explicit SegmentTree(int N)
     {
+
         size = 1;
         while (size < N)
         {
             size *= 2;
         }
-        data.assign(size * 2 + 1, 0);
+        data.assign(size * 2, 0);
     }
 
     void update(int pos, int x)
@@ -24,43 +25,34 @@ public:
         data[idx] = x;
         while (idx > 0)
         {
-            idx /= 2;
-            data[idx] = max(data[idx * 2], data[idx * 2 + 1]);
+            idx = idx / 2;
+            auto l = data[idx * 2];
+            auto r = data[idx * 2 + 1];
+            data[idx] = l + r;
         }
     }
 
-    void print_data()
+    int rsq(int l, int r)
     {
-        for (auto x : data)
-        {
-            cout << x << " ";
-        }
-        cout << endl;
+        return dfs(l, r, 1, size, 1);
     }
-    int rmq(int l, int r)
-    {
-        return query(l, r, 1, size, 1);
-    }
-    int query(int l, int r, int cl, int cr, int cur)
+
+    int dfs(int l, int r, int cl, int cr, int idx)
     {
         auto is_in = (l <= cl) && (cr < r);
         auto is_out = (cr < l) || (r <= cl);
-        // cout << endl;
-        // cout << l << " " << r << " " << cl << " " << cr << endl;
         if (is_in)
         {
-            return data[cur];
+            return data[idx];
         }
         if (is_out)
         {
-            return -1000000000;
+            return 0;
         }
         int mid = (cl + cr) / 2;
-        // cout << (mid + 1) << " " << mid << endl;
-        int max_l = query(l, r, cl, mid, cur*2);
-        int max_r = query(l, r, mid + 1, cr, cur*2+1);
-        int ans = max(max_l, max_r);
-        return ans;
+        int left = dfs(l, r, cl, mid, idx * 2);
+        int right = dfs(l, r, mid+1, cr, idx * 2 + 1);
+        return left + right;
     }
 };
 
@@ -83,7 +75,7 @@ int main()
         {
             int l, r;
             cin >> l >> r;
-            cout << seg.rmq(l, r) << endl;
+            cout << seg.rsq(l, r) << endl;
         }
         // seg.print_data();
     }
