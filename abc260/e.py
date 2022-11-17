a6aa06fe677409e7b08e77fd46918771e0837d4d
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-11-17 11:53:43 +0900

    Solved  abc260 e

---
+++ abc260/e.py
@@ -0,0 +1,24 @@
+N,M = map(int,input().split())
+pair = [0]*(M+1)
+imos = [0]*(M+2)
+Amax = 0
+Bmin = 10**10
+
+for _ in range(N):
+    a,b = map(int,input().split())
+    pair[a] = max(pair[a],b)
+    Amax = max(Amax,a)
+    Bmin = min(Bmin,b)
+
+r = Amax
+for l in range(1,Bmin+1):
+    imos[r-l+1] += 1
+    imos[M-l+2] -= 1
+
+    if pair[l] > 0:
+        r = max(r,pair[l])
+
+for i in range(1,M+1):
+    imos[i] += imos[i-1]
+
+print(*imos[1:M+1])
